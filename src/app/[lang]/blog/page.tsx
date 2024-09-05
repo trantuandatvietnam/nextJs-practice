de6ac9d21b599b86d3c11da09bafae4320a83fec
DAT Tran Tuan <dat.trantuan@vti.com.vn>
2024-09-05 18:02:29 +0700

    update

--- src/app/[lang]/blog/page.tsx
+++ src/app/[lang]/blog/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
-export default function Page() {
-  return <div>My page of blog in top-level</div>;
+export default async function Page() {
+  const data = await fetch('http://localhost:8000/todos', {cache: 'no-store'})
+  let posts = await data.json()
+  return <div>My page of blog in top-level {JSON.stringify(posts)}</div>;
 }
